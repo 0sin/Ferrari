f416d70b8f5d1c0dac423d5b330cdaf92bd025e3
beals1129 <57256728+beals1129@users.noreply.github.com>
2020-11-23 18:02:34 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+var videoNumber = 0;
+
+function mediaChange(videoNumber) {
+  for (i = 0; i < 14; i++) {
+    document.getElementsByClassName('video')[i].src = " ";
+
+
+  }
+  if ( videoNumber = 0 ) {
+    document.getElementsByClassName('video')[videoNumber].src = " videos/auto/mp4/812_Superfast.mp4";
+  }
+  if ( videoNumber = 1 ) {
+    document.getElementsByClassName('video')[videoNumber].src = " videos/auto/mp4/812_Superfast.mp4";
+  }
+  if ( videoNumber = 2 ) {
+    document.getElementsByClassName('video')[videoNumber].src = " videos/auto/mp4/812_Superfast.mp4";
+  }
+  if ( videoNumber = 0 ) {
+    document.getElementsByClassName('video')[videoNumber].src = " videos/auto/mp4/812_Superfast.mp4";
+  }
+  if ( videoNumber = 0 ) {
+    document.getElementsByClassName('video')[videoNumber].src = " videos/auto/mp4/812_Superfast.mp4";
+  }
+  if ( videoNumber = 0 ) {
+    document.getElementsByClassName('video')[videoNumber].src = " videos/auto/mp4/812_Superfast.mp4";
+  }
+  if ( videoNumber = 0 ) {
+    document.getElementsByClassName('video')[videoNumber].src = " videos/auto/mp4/812_Superfast.mp4";
+  }
+  
+}
+//   if ((mediaChange(0) = onmouseover(0))) {
+//     document.getElementById("video_1").src =
+//       "videos/auto/mp4/812_Superfast.mp4";
+//     document.getElementById("video_1").style = "display: none;";
+//   }
+
+//   document.getElementById("video_2").src = "videos/auto/mp4/812_GTS.mp4";
+//   document.getElementById("video_3").src = "videos/auto/mp4/SF90.mp4";
+//   document.getElementById("video_4").src = "videos/auto/mp4/812_Superfast.mp4";
+//   document.getElementById("video_5").src = "videos/auto/mp4/F8_Tributo.mp4";
+//   document.getElementById("video_6").src = "videos/auto/mp4/F8_Spider.mp4";
+//   document.getElementById("video_7").src =
+//     "videos/auto/mp4/Ferrari_488_Pista.mp4";
+//   document.getElementById("video_8").src =
+//     "videos/auto/mp4/Ferrari_488_Pista_Spider.mp4";
+//   document.getElementById("video_9").src = "videos/auto/mp4/Ferrari_Roma.mp4";
+//   document.getElementById("video_10").src =
+//     "videos/auto/mp4/Ferrari_Porotofino.mp4";
+//   document.getElementById("video_11").src =
+//     "videos/auto/mp4/Ferrari_Monza_SP1.mp4";
+//   document.getElementById("video_12").src =
+//     "videos/auto/mp4/Ferrari_Monza_SP2.mp4";
+//   document.getElementById("video_13").src = "videos/auto/mp4/Line_Up.mp4";
+// }
+
+// function mediaDisplay(i) {
+//   document.getElementById("video_1").style = "display: none";
+//   document.getElementById("video_2").style = "display: none";
+//   document.getElementById("video_3").style = "display: none";
+//   document.getElementById("video_4").style = "display: none";
+//   document.getElementById("video_5").style = "display: none";
+//   document.getElementById("video_6").style = "display: none";
+//   document.getElementById("video_7").style = "display: none";
+//   document.getElementById("video_8").style = "display: none";
+//   document.getElementById("video_9").style = "display: none";
+//   document.getElementById("video_10").style = "display: none";
+//   document.getElementById("video_11").style = "display: none";
+//   document.getElementById("video_12").style = "display: none";
+//   document.getElementById("video_13").style = "display: none";
+// }
+
+// mediaChange();
+// mediaDisplay();
