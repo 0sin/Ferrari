e1dc84f2cf11e7a64c9f05e33690fe9ca469c910
beals1129 <57256728+beals1129@users.noreply.github.com>
2020-11-27 01:49:01 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,88 +2,176 @@
 var currentSlideNumber = 0; // 시작 슬라이드 번호를 0 에서 시작
 var slide =document.getElementsByClassName('Theme_warp');
 
+var mainsubscribe = document.getElementsByClassName('main_subscribe');
+var mainTitle = document.getElementsByClassName('main_banner_title');
+var mainSubTitle = document.getElementsByClassName('main_sub_conts');
 
+// 첫 페이지 실행 함수
+function mainFirstPage() {
+    mainsubscribe[0].style="animation: opacity 1s ease forwards; animation-delay: 1s";
+    mainSubTitle[0].style="animation: opacity 1s ease forwards; animation-delay: 1s";
+    for ( i =0; i<mainTitle[0].children.length; i++){
+        mainTitle[0].children[i].style="animation: matrix 1s ease-in forwards; animation-delay:"+(0.03* i)+"s;";
+    }
+
+    document.querySelectorAll('.main_index circle')[0].style = "stroke-width: 0px;"
+    document.querySelectorAll('.main_index circle')[0].style = "stroke: rgb(218, 41, 28); stroke-width: 1px; stroke-dasharray: 70; stroke-dashoffset: 70; animation: wheelbuttondash 6.5s linear forwards;"
+    
+    mainIndexButton();
+
+
+}
+mainFirstPage();
 function moveNext() {
     if (currentSlideNumber !== 2){
         slide[currentSlideNumber].style = " animation : fade_out_rtl 0.4s forwards";
 
+        mainsubscribe[currentSlideNumber].style="animation: opacity_revers 1s ease forwards; animation-delay: 1s";
+        mainSubTitle[currentSlideNumber].style="animation: opacity_revers 1s ease forwards; animation-delay: 1s";
+        for ( i =0; i<mainTitle[currentSlideNumber].children.length; i++){
+            mainTitle[currentSlideNumber].children[i].style="animation: matrix_out 1s ease-in forwards; animation-delay:"+(0.03* i)+"s;";
+        }
+
+
     slide[currentSlideNumber+1].style = "animation : fade_in_rtl 0.4s forwards";
+    mainsubscribe[currentSlideNumber+1].style="animation: opacity 1s ease forwards; animation-delay: 1s";
+        mainSubTitle[currentSlideNumber+1].style="animation: opacity 1s ease forwards; animation-delay: 1s";
 
+    for ( i =0; i<mainTitle[currentSlideNumber+1].children.length; i++){
+        mainTitle[currentSlideNumber+1].children[i].style="animation: matrix 1s ease-in forwards; animation-delay:"+(0.03* i)+"s;";
+    }
 
     currentSlideNumber++
+    mainIndexButton();
+    mainSlideRestart();
     } else {
-        slide[currentSlideNumber].style = " animation : fade_out_rtl 0.4s forwards"
+        slide[currentSlideNumber].style = " animation : fade_out_rtl 0.4s forwards";
         
-        slide[0].style = "animation: fade_in_rtl 0.4s forwards"
-        currentSlideNumber = 0;
-    }
-
-
-}
-
-
-
-function movePrev() {
-    if (currentSlideNumber !== 2){
-        slide[currentSlideNumber].style = " animation : fade_out_ltr 0.4s forwards"
-
-    slide[currentSlideNumber+1].style = "animation : fade_in_ltr 0.4s forwards"
-
-    currentSlideNumber++
-    } else {
-        slide[currentSlideNumber].style = " animation : fade_out_ltr 0.4s forwards"
-        slide[0].style = "animation: fade_in_ltr 0.4s forwards"
-        currentSlideNumber = 0;
+        slide[0].style = "animation: fade_in_rtl 0.4s forwards";
+        mainsubscribe[currentSlideNumber].style="animation: opacity_revers 1s ease forwards; animation-delay: 1s";
+        mainSubTitle[currentSlideNumber].style="animation: opacity_revers 1s ease forwards; animation-delay: 1s";
+        for ( i =0; i<mainTitle[currentSlideNumber].children.length; i++){
+            mainTitle[currentSlideNumber].children[i].style="animation: matrix_out 1s ease-in forwards; animation-delay:"+(0.03* i)+"s;";
+        }
+        mainsubscribe[0].style="animation: opacity 1s ease forwards; animation-delay: 1s";
+        mainSubTitle[0].style="animation: opacity 1s ease forwards; animation-delay: 1s";
+        for ( i =0; i<mainTitle[0].children.length; i++){
+            mainTitle[0].children[i].style="animation: matrix 1s ease-in forwards; animation-delay:"+(0.03* i)+"s;";
+        }
+        currentSlideNumber=0;
+        mainIndexButton();
+        mainSlideRestart();
     }
     
 
+
 }
 
 
+// NEWS INDEX SLIDE
+function mainIndexChange(mainIndexNumber) {
+
+    //방향 정하기
+    var mainDirection =mainIndexNumber-currentSlideNumber ;
+    var mainDistance = Math.abs(mainDirection);
 
 
-setInterval(function() {
+  
+    if (mainDirection > 0) { //양수면 왼쪽으로 전진
+        for(i=0; i<mainTitle[currentSlideNumber].children.length; i++){
+            mainTitle[currentSlideNumber].children[i].style="animation: matrix_out 1s ease-in forwards;";   
+        }
+       
+      for(i=0; i<mainDistance; i++) {
+  
+  
+        //FADE OUT
+        //fade out 애니를 넣어야 fade in 됨
+        slide[currentSlideNumber + i].style = "animation: fade_out_ltr 0.4s forwards;";
+        mainsubscribe[currentSlideNumber + i].style="animation: opacity_revers 1s ease forwards; animation-delay: 1s";
+        mainSubTitle[currentSlideNumber + i].style="animation: opacity_revers 1s ease forwards; animation-delay: 1s";
 
-    moveNext()
+      
+
+        // mainsubscribe[currentSlideNumber+i].style="animation: opacity_delay 6s ease infinite;"
+
+  
+  
+        //FADE IN
+        slide[mainIndexNumber].style = "animation: fade_in_ltr 0.4s forwards";
+        mainsubscribe[mainIndexNumber].style="animation: opacity 1s ease forwards; animation-delay: 1s";
+        mainSubTitle[mainIndexNumber].style="animation: opacity 1s ease forwards; animation-delay: 1s";
+        for ( i =0; i<mainTitle[mainIndexNumber].children.length; i++){
+            
+            mainTitle[mainIndexNumber].children[i].style="animation: matrix 1s ease-in forwards; animation-delay:"+(0.03* i)+"s;";
+            // mainTitle[currentSlideNumber + i].children[i].style="animation: matrix_out 1s ease-in forwards;animation-delay:"+(0.03* i)+"s;";
+        }
+
+  
+  
+       currentSlideNumber = mainIndexNumber ;
+        mainIndexButton();
+        mainSlideRestart();
+      }
+    }
+  
+    else if (mainDirection < 0) { //음수면 오른쪽으로 전진
+  
+        for(i=0; i<mainTitle[currentSlideNumber].children.length; i++){
+            mainTitle[currentSlideNumber].children[i].style="animation: matrix_out 1s ease-in forwards;";   
+        }
+        //FADE OUT
+        slide[currentSlideNumber].style = "animation: fade_out_rtl 0.4s forwards;";
+        mainsubscribe[currentSlideNumber].style="animation: opacity_revers 1s ease forwards; animation-delay: 1s";
+        mainSubTitle[currentSlideNumber].style="animation: opacity_revers 1s ease forwards; animation-delay: 1s";
+        // mainsubscribe[currentSlideNumber+i].style="animation: opacity_delay 6s ease infinite;"
+
+  
+  
+        //FADE IN
+        slide[mainIndexNumber].style = "animation: fade_in_rtl 0.4s forwards";
+        mainsubscribe[mainIndexNumber].style="animation: opacity 1s ease forwards; animation-delay: 1s";
+        mainSubTitle[mainIndexNumber].style="animation: opacity 1s ease forwards; animation-delay: 1s";
+        for ( i =0; i<mainTitle[mainIndexNumber].children.length; i++){
+            mainTitle[mainIndexNumber].children[i].style="animation: matrix 1s ease-in forwards; animation-delay:"+(0.03* i)+"s;";
+        }
+  
+  
+        currentSlideNumber = mainIndexNumber ;
+        mainIndexButton();
+        mainSlideRestart();
+     }
     
-    
-}, 6000);
-;
+  }
+  
+  
+  // NEWS INDEX BUTTON STYLE
+  
+  function mainIndexButton(buttonNumber) {
+    for (i=0; i<slide.length; i++) {
+      document.querySelectorAll('.main_index circle')[i].style = "stroke-width: 0px;"
+    }
+    document.querySelectorAll('.main_index circle')[currentSlideNumber].style = "stroke: rgb(218, 41, 28); stroke-width: 1px; stroke-dasharray: 70; stroke-dashoffset: 70; animation: wheelbuttondash 6.5s linear forwards;"
+  }
+  
+  
+  
+  // AUTOPLAY
+  
+  var mainLoopState = 1;
+  var mainSlideLoop = setInterval(moveNext, 7000);
+  
+  function mainSlideRestart() {
+    if (mainLoopState == 1) {
+      clearInterval(mainSlideLoop);
+      mainSlideLoop = setInterval(moveNext, 7000);
+    }
+  }
 
-// 작은 원 3개 순서대로 움직이는 스크립트 
-    setTimeout(redCircleOne,0)
-    setTimeout(redCircleTwo, 6100);
-    setTimeout(redCircleThree, 12000);
-
-    setInterval(function(){
-        setTimeout(redCircleOne,0)
-    setTimeout(redCircleTwo, 6000);
-    setTimeout(redCircleThree, 12000);
-    }, 18000);
 
 
-var redCircle = document.getElementsByClassName('swipper_red');
 
-function redCircleOne(){
 
-    var style = redCircle[0].style;
-    style.webkitAnimationPlayState = 'running';
-
-    What = setTimeout(function(){style.webkitAnimationPlayState = 'paused';},6000);
-    
-}
-function redCircleTwo(){
-
-var styleTwo = redCircle[1].style;
-styleTwo.webkitAnimationPlayState = 'running';
-setTimeout(function(){styleTwo.webkitAnimationPlayState = 'paused';},6000);
-}
-function redCircleThree(){
-
-var styleThree = redCircle[2].style;
-styleThree.webkitAnimationPlayState = 'running';
-setTimeout(function(){styleThree.webkitAnimationPlayState = 'paused';},6000);
-}
 
 
 
